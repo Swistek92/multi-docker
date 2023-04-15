31db7ebd41a9ff586ecb0eb705b7cdaf038b8f22
Piotrek <Swistekxd@gmail.com>
2023-04-15 20:29:08 +0200

    hello

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,13 @@ function App() {
   return (
     <Router>
       <div className='App'>
+        <h1>hello AWS world </h1>
+        <img
+          className='img'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/480px-The_Blue_Marble_%28remastered%29.jpg'
+          alt='world'
+        />
+        <br />
         <Link to='/'>Home</Link>
         <br />
         <Link to='/otherpage'>Other Page</Link>
